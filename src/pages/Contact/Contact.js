875c0de6cfe58de0a0944277226550eeaf49e070
Yadav <dyadav.contractor@rti.org>
2020-01-22 16:10:22 +0530

    prettier eslint added

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import  {Card }  from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
 
 function Contact() {
-    return (
-        <div>
-          <h1>Contact page</h1>  
+  return (
+    <div>
+      <h1>Contact page</h1>
       <Card>
-  <Card.Body>This is some text within a card body.</Card.Body>
-</Card>
-        </div>
-    )
+        <Card.Body>This is some text within a card body.</Card.Body>
+      </Card>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
